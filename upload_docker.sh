85288104009fb35e051cc3b59dcecb22945c9996
ahmedhasandrlnd <hasan.ahmedmonjurul@usask.ca>
2020-09-05 22:26:31 +0000

    upload_docker updated

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 249267795339.dkr.ecr.us-west-2.amazonaws.com
+docker build -t devops_project .
+docker tag devops_project:latest 249267795339.dkr.ecr.us-west-2.amazonaws.com/devops_project:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push 249267795339.dkr.ecr.us-west-2.amazonaws.com/devops_project:latest
